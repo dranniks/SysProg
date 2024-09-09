eb20a24f0d6312d0fd88a9fd95372687afe49dd1
dranniks <136456377+dranniks@users.noreply.github.com>
2024-09-09 13:34:14 +0000

    Lab_1

---
+++ Lab_1/sandbox/C_version.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main() {
+    printf("Talaev \nAndrey \nPetrovich\n");
+    return 0;
+}
---
+++ Lab_1/sandbox/a.asm
@@ -0,0 +1,15 @@
+format ELF
+public _start
+msg db "Talaev", 0xA, "Andrey", 0xA, "Petrovich", 0xA, 0
+
+_start:
+    ;инициализация регистров для вывода информации на экран
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, msg
+    mov edx, 26
+    int 0x80
+    ;инициализация регистров для успешного завершения работы программы
+    mov eax, 1
+    mov ebx, 0
+    int 0x80
