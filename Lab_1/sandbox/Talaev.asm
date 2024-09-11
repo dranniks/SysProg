96f6756940921f5b236c5afc2e9aac18cb2dde79
dranniks <136456377+dranniks@users.noreply.github.com>
2024-09-11 17:13:50 +0000

    correction errors

--- Lab_1/sandbox/Talaev.asm
+++ Lab_1/sandbox/Talaev.asm
@@ -1,15 +1,15 @@
-format ELF64
+format ELF
 public _start
 msg db "Talaev", 0xA, "Andrey", 0xA, "Petrovich", 0xA, 0
 
 _start:
     ;инициализация регистров для вывода информации на экран
-    mov rax, 4
-    mov rbx, 1
-    mov rcx, msg
-    mov rdx, 25
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, msg
+    mov edx, 25
     int 0x80
     ;инициализация регистров для успешного завершения работы программы
-    mov rax, 1
-    mov rbx, 0
+    mov eax, 1
+    mov ebx, 0
     int 0x80
